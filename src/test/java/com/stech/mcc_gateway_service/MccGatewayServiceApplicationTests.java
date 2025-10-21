8675459456624ab91b1e33490aa890be76528f83
juancarlossuarezmarin <juan.suarez@pragma.com.co>
2025-10-20 21:26:00 -0500

    create mc-gateway-customer

---
+++ src/test/java/com/stech/mcc_gateway_service/MccGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stech.mcc_gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MccGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
